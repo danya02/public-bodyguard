043e586285b6e6b6a5a1e5830249bb43b0823bbb
Danya Generalov <danya02@me.com>
2016-08-03 13:30:34 +0300

    Lint operation.

--- server.py
+++ server.py
@@ -32,9 +32,9 @@ global log
 try:
     log = json.load(open(conf["path_to_log"]))["log"]
 except:
-    log=[]
+    log = []
 global messages
-messages=0
+messages = 0
 m = mqtt.Client()
 m.connect("127.0.0.1")
 for i in ("data_chan", "event_chan", "reply_chan", "cancel_chan"):
@@ -43,39 +43,39 @@ class Event:
     """This class defines an Event."""
     def __setattr__(self, name, value):
         object.__setattr__(self, name, value)
-        if name=="long":
+        if name == "long":
             object.__setattr__(self, "location", [location[0], value])
-        elif name=="lat":
+        elif name == "lat":
             object.__setattr__(self, "location", [value, location[1]])
-        elif name=="location":
+        elif name == "location":
             object.__setattr__(self, "lat", value[0])
             object.__setattr__(self, "long", value[1])
 
 
     def import_dict(self, dic):
-        self.euid=dic["euid"]
-        self.uuid=dic["uuid"]
-        self.timestamp=dic["timestamp"]
-        self.level=dic["level"]
-        self.location=dic["location"]
-        self.long=self.location[1]
-        self.lat=self.location[0]
+        self.euid = dic["euid"]
+        self.uuid = dic["uuid"]
+        self.timestamp = dic["timestamp"]
+        self.level = dic["level"]
+        self.location = dic["location"]
+        self.long = self.location[1]
+        self.lat = self.location[0]
 
 
     def export_dict(self):
-        dic=dict()
+        dic = dict()
         for i in ("euid", "uuid", "timestamp", "level", "location"):
-            dic[i]=object.__getattr__(self, i)
+            dic[i] = object.__getattr__(self, i)
         return dic
 
 
     def __init__(self, dic=None):
-        self.level=0
-        self.lat=0
-        self.long=0
-        self.timestamp=0
-        self.uuid="00000000-0000-0000-0000-000000000000"
-        self.euid=self.uuid
+        self.level = 0
+        self.lat = 0
+        self.long = 0
+        self.timestamp = 0
+        self.uuid = "00000000-0000-0000-0000-000000000000"
+        self.euid = self.uuid
         if dic is not None:
             if not isinstance(dic, dict):
                 raise TypeError("Expected type dict, but got "+str(type(dic))+" instead")
@@ -102,8 +102,8 @@ def cancelmoose():
                 if conf["debug"]:
                     print("Event of level "+i.level+" from user ID "+i.uuid+" with ID "+i.euid+" is canceled because of request")
                 events.remove(i)
-cancelmoose_thread=threading.Thread(target=cancelmoose, name="cancelmoose")
-cancelmoose_thread.daemon=True
+cancelmoose_thread = threading.Thread(target=cancelmoose, name="cancelmoose")
+cancelmoose_thread.daemon = True
 cancelmoose_thread.start()
 
 def parser(client, userdata, msg):
@@ -111,24 +111,24 @@ def parser(client, userdata, msg):
     global messages
     global events
     global to_cancel
-    messages+=1
-    log=log+[{"topic":msg.topic, "payload":msg.payload}]
-    if messages==conf["limit_to_save"]+1:
-        messages=0
+    messages += 1
+    log = log+[{"topic":msg.topic, "payload":msg.payload}]
+    if messages == conf["limit_to_save"]+1:
+        messages = 0
         json.dump({"log":log}, open(conf["path_to_log"],"w"))
         json.dump({"events":events}, open(conf["path_to_event_list"],"w"))
-    if msg.topic==conf["data_chan"]:
+    if msg.topic == conf["data_chan"]:
         open(conf["path_to_data_folder"]+msg.payload.partition(
             "::")[0],"w").write(msg.payload.partition("::")[2])
-    elif msg.topic==conf["event_chan"]:
-        message=json.loads(msg.payload)
+    elif msg.topic == conf["event_chan"]:
+        message = json.loads(msg.payload)
         message.update({"timestamp":time.time()})
-        message=Event(message)
+        message = Event(message)
         if conf["debug"]:
             print("Recieved event of level "+message.level+" from user ID "+message.uuid+", which was assigned ID "+message.euid)
-        events=events+[message]
-    elif msg.topic==conf["cancel_chan"]:
+        events = events+[message]
+    elif msg.topic == conf["cancel_chan"]:
         for i in events:
-            if msg.payload.split("::")[0]==i["uuid"] and msg.payload.split("::")[1]==i["euid"]:
-                to_cancel=to_cancel+[i]
+            if msg.payload.split("::")[0] == i["uuid"] and msg.payload.split("::")[1] == i["euid"]:
+                to_cancel = to_cancel+[i]
 m.on_message = parser
